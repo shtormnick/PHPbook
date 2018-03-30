961a07cdec56e2fd0ac326dff82aa70fbf96fdc4
shtormnick <loxred@mail.ru>
2018-03-30 17:43:06 +0300

    Update for.php

--- 04_Task/for.php
+++ 04_Task/for.php
@@ -8,6 +8,8 @@
          for( $i = 0; $i<5; $i++ ) {
             $a += 10;
             $b += 5;
+            echo $a."</br>";
+            echo $b."</br>";
          }
 
          echo ("At the end of the loop a = $a and b = $b" );
