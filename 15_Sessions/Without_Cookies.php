45e8f8f2e0533ac7e64aa65bba19e7fc9af9fae7
lorix <foxred324@gmail.com>
2018-05-01 09:37:42 +0300

    11_11

---
+++ 15_Sessions/Without_Cookies.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+
+    if (isset($_SESSION['counter'])) {
+        $_SESSION['counter'] = 1;
+    }else {
+        $_SESSION['counter']++;
+    }
+
+    $msg = "You have visited this page ".  $_SESSION['counter'];
+    $msg .= "in this session.";
+
+    echo ( $msg );
+?>
+
+<p>
+    To continue  click following link <br />
+
+    <a  href = "nextpage.php?<?php echo htmlspecialchars(SID); ?>">
+</p>
