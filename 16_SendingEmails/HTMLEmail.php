e489e4c2d59bf1db984b0c555b689a5a270b58f6
lorix <foxred324@gmail.com>
2018-06-04 22:06:44 +0300

    11_17

--- 16_SendingEmails/HTMLEmail.php
+++ 16_SendingEmails/HTMLEmail.php
@@ -7,16 +7,16 @@
     <body>
 
         <?php
-            $to = "xyz@somedomain.com";
+            $to = "foxred324@gmail.com";
             $subject = "This is subject";
 
             $message = "<b>This is HTML message.</b>";
             $message .= "<h1>This is headline.</h1>";
 
-            $header = "From:abc@somedomain.com \r\n";
-            $header .= "Cc:afgh@somedomain.com \r\n";
+            $header = "From:foxred324@gmail.com \r\n";
+            $header .= "Cc:vasdasd@gmail.com \r\n";
             $header .= "MIME-Version: 1.0\r\n";
-            $header .= "Content-type: text/html\r\n";
+            $header .= "Content-type: text/html; charset=windows-1251\r\n";
 
             $retval = mail ($to,$subject,$message,$header);
 
